4b40d24c2edab0dd58110de1877e9704c112e875
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-04 17:49:10 +0530

    added numbers

--- game.js
+++ game.js
@@ -14,27 +14,32 @@
 	        
 	        for(var j = 0; j < 9; j++) {
 	            var spot = {};
-	            spot.isCovered = true;
+	            spot.isRevealed = false;
 	            spot.content="empty";
 	            row.spots.push(spot);
 	        }
 	        minefield.rows.push(row);
 	    }
-	    for(var m=0; m<10; m++){
+	    for(var m=0; m<12; m++){
 	    	placeRandomMine(minefield);
 	    }
+
+	    for(var x=0; x<9; x++){
+	    	for(var y=0; y<9; y++){
+	    		calculateNumber(minefield, x, y);
+	    	}
+	    }
+
 	    return minefield;
 	}
 
 	function placeRandomMine(minefield){
-		var row = Math.floor(Math.random()*9);
-		var column=Math.floor(Math.random()*9);
-		var spot = getSpot(minefield, row, column);
-		var flag = 0;
-		while(!flag){
+		while(true){
+			var row = Math.floor(Math.random()*9);
+			var column=Math.floor(Math.random()*9);
 			spot = getSpot(minefield, row, column);
 			if(spot.content="empty"){
-				flag=1;
+				break;
 			}
 		}
 		spot.content="bomb";		
@@ -42,6 +47,67 @@
 
 	function getSpot(minefield, row, column){
 		return minefield.rows[row].spots[column];
+	}
+
+	function calculateNumber(minefield, row, column) {
+	    var thisSpot = getSpot(minefield, row, column);
+	    if(thisSpot.content == "bomb") {
+	        return;
+	    }
+	    
+	    var mineCount = 0;
+	    if(row > 0) {
+	        if(column > 0) {
+	            var spot = getSpot(minefield, row - 1, column - 1);
+	            if(spot.content == "bomb") {
+	                mineCount++;
+	            }
+	        }
+	        var spot = getSpot(minefield, row - 1, column);
+	        if(spot.content == "bomb") {
+	            mineCount++;
+	        }
+	        if(column < 8) {
+	            var spot = getSpot(minefield, row - 1, column + 1);
+	            if(spot.content == "bomb") {
+	                mineCount++;
+	            }
+	        }
+	    }
+	    if(column > 0) {
+	        var spot = getSpot(minefield, row, column - 1);
+	        if(spot.content == "bomb") {
+	            mineCount++;
+	        }
+	    }
+	    if(column < 8) {
+	        var spot = getSpot(minefield, row, column + 1);
+	        if(spot.content == "bomb") {
+	            mineCount++;
+	        }
+	    }
+	    if(row < 8) {
+	        if(column > 0) {
+	            var spot = getSpot(minefield, row + 1, column - 1);
+	            if(spot.content == "bomb") {
+	                mineCount++;
+	            }
+	        }
+	        var spot = getSpot(minefield, row + 1, column);
+	        if(spot.content == "bomb") {
+	            mineCount++;
+	        }
+	        if(column < 8) {
+	            var spot = getSpot(minefield, row + 1, column + 1);
+	            if(spot.content == "bomb") {
+	                mineCount++;
+	            }
+	        }
+	    }
+	    
+	    if(mineCount > 0) {
+	        thisSpot.content = mineCount;
+	    }
 	}
 
 })();
