a08da791f90d0563f3ca5f09896cb8916038b84c
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-04 15:36:55 +0530

    added mines

--- game.js
+++ game.js
@@ -15,13 +15,33 @@
 	        for(var j = 0; j < 9; j++) {
 	            var spot = {};
 	            spot.isCovered = true;
+	            spot.content="empty";
 	            row.spots.push(spot);
 	        }
-	        
 	        minefield.rows.push(row);
 	    }
-	    
+	    for(var m=0; m<10; m++){
+	    	placeRandomMine(minefield);
+	    }
 	    return minefield;
+	}
+
+	function placeRandomMine(minefield){
+		var row = Math.floor(Math.random()*9);
+		var column=Math.floor(Math.random()*9);
+		var spot = getSpot(minefield, row, column);
+		var flag = 0;
+		while(!flag){
+			spot = getSpot(minefield, row, column);
+			if(spot.content="empty"){
+				flag=1;
+			}
+		}
+		spot.content="bomb";		
+	}
+
+	function getSpot(minefield, row, column){
+		return minefield.rows[row].spots[column];
 	}
 
 })();
